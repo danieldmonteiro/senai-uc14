3c5d7ed4f033f5a374f977ed90342259d70d436d
danieldmonteiro <danieldmonteiro@gmail.com>
2022-08-30 19:38:28 -0300

    Encontro Remoto 2

--- API-Livros/API-Livros/Controllers/LivroController.cs
+++ API-Livros/API-Livros/Controllers/LivroController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using API_Livros.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using API_Livros.Models;
 
 namespace API_Livros.Controllers
 {
@@ -33,6 +34,74 @@ namespace API_Livros.Controllers
                 throw new Exception(e.Message);
             }
         }
+
+        [HttpGet("{id}")]
+
+        public IActionResult BuscarPorId(int id)
+        {
+            try
+            {
+               Livro livroBuscado = _livroRepository.BuscarPorId(id);
+
+                if(livroBuscado == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(livroBuscado);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpPost]
+
+        public IActionResult Cadastrar(Livro l)
+        {
+            try
+            {
+                _livroRepository.Cadastrar(l);
+
+                return StatusCode(201);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+
+        public IActionResult Deletar(int id)
+        {
+            try
+            {
+                _livroRepository.Deletar(id);
+                return Ok("Livro removido com sucesso");
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+
+        public IActionResult Alterar(int id, Livro l)
+        {
+            try
+            {
+                _livroRepository.Alterar(id, l);
+                return StatusCode(204);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
 
     }
 }
